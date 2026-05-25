d32c36fca5e8adb8366d22ced66eb08af0890b9e
winner10920 <74070990+winner10920@users.noreply.github.com>
2026-05-25 18:46:30 -0400

    Update setup_influxdb.sh

--- scripts/setup_influxdb.sh
+++ scripts/setup_influxdb.sh
@@ -9,17 +9,15 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
-# Check if argument exists before accessing \$1
+# Check if argument exists before accessing $1
 if [ $# -ge 1 ] && [ "$1" == "--uninstall" ]; then
     exit 0
 fi
 
 echo "Setup influxdb install source..."
-# influxdata-archive.key GPG fingerprint:
-#   Primary key fingerprint: 24C9 75CB A61A 024E E1B6  3178 7C3D 5715 9FC2 F927
-#   Subkey fingerprint:      9D53 9D90 D332 8DC7 D6C8  D3B9 D8FF 8E1F 7DF8 B07E
 curl --silent --location -O https://repos.influxdata.com/influxdata-archive.key
-gpg --show-keys --with-fingerprint --with-colons ./influxdata-archive.key 2>&1 && cat influxdata-archive.key | gpg --dearmor | sudo tee /etc/apt/keyrings/influxdata-archive.gpg > /dev/null
-echo 'deb [signed-by=/etc/apt/keyrings/influxdata-archive.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
+
+cat influxdata-archive.key | gpg --dearmor | tee /etc/apt/trusted.gpg.d/influxdata-archive.gpg > /dev/null
+echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive.gpg] https://repos.influxdata.com/debian stable main' | tee /etc/apt/sources.list.d/influxdata.list
+
 rm influxdata-archive.key
-apt-get update
