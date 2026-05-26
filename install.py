769cc12d07784a4068c084d803e1aa5ca782d442
winner10920 <74070990+winner10920@users.noreply.github.com>
2026-05-25 22:03:44 -0400

    Update install.py

--- install.py
+++ install.py
@@ -62,7 +62,7 @@ settings = {
     # - Install python source code from git
     'python_source': {
         'pironman5': './',
-        'pm_auto': f'git+https://github.com/winner10920/pm_auto.git@v2',
+        'pm_auto': f'git+https://github.com/winner10920/pm_auto.git@1.2.12',
     },
 
     # - Setup config txt
