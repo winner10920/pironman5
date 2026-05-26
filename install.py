da1e02456666353b18e935f0b7adc6264152a6d3
winner10920 <74070990+winner10920@users.noreply.github.com>
2026-05-26 14:04:26 -0400

    Update pm_auto git reference to version 1.2.x

--- install.py
+++ install.py
@@ -62,7 +62,7 @@ settings = {
     # - Install python source code from git
     'python_source': {
         'pironman5': './',
-        'pm_auto': f'git+https://github.com/winner10920/pm_auto.git@1.2.12',
+        'pm_auto': f'git+https://github.com/winner10920/pm_auto.git#1.2.x',
     },
 
     # - Setup config txt
